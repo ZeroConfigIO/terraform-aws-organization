57b260baa2df6ddf3d254ef215bf01b7b534bbcf
Igor J. Santos <igor@igorjs.io>
2022-01-27 00:34:19 +1100

    chore(*): add example

---
+++ examples/basic/main.tf
@@ -0,0 +1,36 @@
+locals {
+  tags = {
+    "provisioned" = "terraform"
+  }
+
+  organization_units = [
+    "security",
+    "stage",
+    "production"
+  ]
+
+  organization_accounts = {
+    security = {
+      email       = "aws+security@igorjs.io",
+      parent_name = "sec",
+      tags        = local.tags
+    },
+    stage = {
+      email     = "aws+development@igorjs.io",
+      parent_id = "dev",
+      tags      = local.tags
+    },
+    production = {
+      email     = "aws+production@igorjs.io",
+      parent_id = "prod",
+      tags      = local.tags
+    },
+  }
+}
+
+module "organization" {
+  source                = "../../"
+  organization_accounts = local.organization_accounts
+  organization_units    = local.organization_units
+  organization_aws_service_access_principals = local.organization_aws_service_access_principals
+}
